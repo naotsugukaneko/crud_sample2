81dc4fbb51adf762ceaab9c90a10bc5002ceb410
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-05-10 00:21:32 +0000

    編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
     # redirect_to action: index
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+  
   def destroy
     user = User.find(params[:id])
     user.destroy
